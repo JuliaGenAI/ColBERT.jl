cf1d08c846ae9ef371a0405b95537908e29d4209
Documenter.jl <documenter@juliadocs.github.io>
2024-05-06 08:55:26 +0000

    build based on 88d5e2a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ColBERT","category":"page"},{"location":"#ColBERT","page":"Home","title":"ColBERT","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ColBERT.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ColBERT]","category":"page"}]
+}
