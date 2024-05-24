aea3100ed2e944f9696f24af1c8b29e3fa8390ce
Siddhant Chaudhary <urssidd@gmail.com>
2024-05-24 11:33:32 +0530

    Standard includes.

--- src/ColBERT.jl
+++ src/ColBERT.jl
@@ -1,3 +1,15 @@
 module ColBERT
 
+# datasets
+include("data/collection.jl")
+include("data/queries.jl")
+
+# config and other infra
+include("infra/settings.jl")
+include("infra/config.jl")
+
+# models
+include("modelling/checkpoint.jl")
+include("modelling/colbert.jl")
+
 end
