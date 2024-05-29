5d11786afae7e4dd6ec035f67c49928ca363d07b
Siddhant Chaudhary <urssidd@gmail.com>
2024-05-29 20:24:13 +0530

    Creating a smaller dataset for testing.

--- examples/lotte.sh
+++ examples/lotte.sh
@@ -1,2 +1,3 @@
 wget https://downloads.cs.stanford.edu/nlp/data/colbert/colbertv2/lotte.tar.gz -P downloads/
 tar -xvzf downloads/lotte.tar.gz -C downloads/
+head downloads/lotte/lifestyle/dev/collection.tsv > downloads/lotte/lifestyle/dev/short_collection.tsv
