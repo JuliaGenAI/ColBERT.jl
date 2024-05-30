66642b3a3ad446f91da9dd502d15a76953f8044f
Siddhant Chaudhary <urssidd@gmail.com>
2024-05-30 11:09:17 +0530

    Updating the example.

--- examples/data.jl
+++ examples/data.jl
@@ -3,7 +3,37 @@ using ColBERT
 dataroot = "downloads/lotte"
 dataset = "lifestyle"
 datasplit = "dev"
-path = joinpath(dataroot, dataset, datasplit, "collection.tsv")
+path = joinpath(dataroot, dataset, datasplit, "short_collection.tsv")
 
 collection = Collection(path)
+length(collection.data)
 
+nbits = 2   # encode each dimension with 2 bits
+doc_maxlen = 300   # truncate passages at 300 tokens
+
+checkpoint = "colbert-ir/colbertv2.0"                       # the HF checkpoint
+index_root = "experiments/notebook/indexes"
+index_name = "short_$(dataset).$(datasplit).$(nbits)bits"
+index_path = joinpath(index_root, index_name)
+
+config = ColBERTConfig(
+    RunSettings(
+        experiment="notebook",
+    ),
+    TokenizerSettings(),
+    ResourceSettings(
+        checkpoint=checkpoint,
+        collection=collection,
+        index_name=index_name,
+    ),
+    DocSettings(
+        doc_maxlen=doc_maxlen,
+    ),
+    QuerySettings(),
+    IndexingSettings(
+        index_path=index_path,
+        nbits=nbits,
+        kmeans_niters=20,
+    ),
+    SearchSettings(),
+)
