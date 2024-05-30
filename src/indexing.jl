dfc32697ff9ed6891b21ffa3ceb45c25f6c574fb
Siddhant Chaudhary <urssidd@gmail.com>
2024-05-30 11:23:05 +0530

    Adding the template for the `index()` call.

--- src/indexing.jl
+++ src/indexing.jl
@@ -2,8 +2,14 @@ using .ColBERT: ColBERTConfig
 
 struct Indexer
     # index_path::String            we can just reuse the path from the config?
-    checkpoint::String
+    # checkpoint::String            can also infer this from config
     config::ColBERTConfig
 end
 
+function index(indexer::Indexer)
+    if isdir(indexer.config.index_path)
+        @info "Index at $(indexer.config.index_path) already exists!"
+    end
 
+    # TODO: finish this!
+end
