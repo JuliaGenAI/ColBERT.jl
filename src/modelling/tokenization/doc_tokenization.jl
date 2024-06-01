cf9e47f797337b5970e185589d5dba75683066db
Siddhant Chaudhary <urssidd@gmail.com>
2024-06-01 21:09:37 +0530

    Adding implementation of utility functions, `_sort_by_length` and `_split_into_batches`.

--- src/modelling/tokenization/doc_tokenization.jl
+++ src/modelling/tokenization/doc_tokenization.jl
@@ -34,7 +34,41 @@ function tensorize(doc_tokenizer::DocTokenizer, tokenizer::Transformers.TextEnco
     end
 end
 
+"""
+    _sort_by_length(ids::AbstractMatrix, mask::AbstractMatrix, bsize::Int)
 
+Sort sentences by number of attended tokens, if the number of sentences is larger than bsize. If the number of passages (first dimension of `ids`) is atmost
+than `bsize`, the `ids`, `mask`, and a list `Vector(1:size(ids)[1])` is returned as a three-tuple. Otherwise,
+the passages are first sorted by the number of attended tokens (figured out from `mask`), and then the the sorted arrays
+`ids` and `mask` are returned, along with a reversed list of indices, i.e a mapping from passages to their indice in the sorted list.
+"""
+function _sort_by_length(integer_ids::AbstractMatrix, integer_mask::AbstractMatrix, bsize::Int)
+    batch_size = size(integer_ids)[2]
+    if batch_size <= bsize
+        # if the number of passages fits the batch size, do nothing
+        return integer_ids, integer_mask, Vector(1:batch_size)
+    end
+
+    lengths = vec(sum(integer_mask; dims = 1))              # number of attended tokens in each passage
+    indices = sortperm(lengths)                     # get the indices which will sort lengths
+    reverse_indices = sortperm(indices)             # invert the indices list
+
+    integer_ids[:, indices], integer_mask[:, indices], reverse_indices
+end
+
+"""
+    _split_into_batches(integer_ids::AbstractArray, integer_mask::AbstractMatrix, bsize::Int)::Vector{Tuple{AbstractArray, AbstractMatrix, Int}}
+
+Split the given `integer_ids` and `integer_mask` into batches of size `bsize`.
+"""
+function _split_into_batches(integer_ids::AbstractArray, integer_mask::AbstractMatrix, bsize::Int)
+    batch_size = size(integer_ids)[2]
+    batches = Vector{Tuple{AbstractArray, AbstractMatrix}}()
+    for offset in 1:bsize:batch_size
+        push!(batches, (integer_ids[:, offset:min(batch_size, offset + bsize - 1)], integer_mask[:, offset:min(batch_size, offset + bsize - 1)]))
+    end
+    batches
+end
 
 # tokenizer = base_colbert.tokenizer
 # batch_text = [
