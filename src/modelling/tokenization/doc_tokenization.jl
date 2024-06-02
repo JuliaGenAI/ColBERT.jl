a9b266d340202e91418092a50cff957c9c99bb39
Siddhant Chaudhary <urssidd@gmail.com>
2024-06-02 19:13:51 +0530

    An informative comment.

--- src/modelling/tokenization/doc_tokenization.jl
+++ src/modelling/tokenization/doc_tokenization.jl
@@ -27,6 +27,7 @@ function tensorize(doc_tokenizer::DocTokenizer, tokenizer::Transformers.TextEnco
     if ismissing(bsize)
         return integer_ids, integer_mask
     else
+        # we sort passages by length to do batch packing for more efficient use of the GPU
         integer_ids, integer_mask, reverse_indices = _sort_by_length(integer_ids, integer_mask, bsize)
         batches = _split_into_batches(integer_ids, integer_mask, bsize)
 
