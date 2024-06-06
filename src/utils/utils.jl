cdea91e930c469f02c787db9b16bb5e05371fff7
Siddhant Chaudhary <urssidd@gmail.com>
2024-06-06 16:07:24 +0530

    Minor fixes in the `batch` function.

--- src/utils/utils.jl
+++ src/utils/utils.jl
@@ -1,15 +1,16 @@
-function batch(group::Vector, bsize::Int, provide_offset = false)
+function batch(group::Vector, bsize::Int; provide_offset::Bool = false)
     vtype = provide_offset ? 
         Vector{Tuple{Int, typeof(group)}} :
         Vector{typeof(group)} 
     batches = vtype()
-    offset = 0
+    offset = 1
     while offset <= length(group)
         if provide_offset
-            push!(batches, (offset, group[offset:offset + bsize - 1]))
+            push!(batches, (offset, group[offset:min(length(group), offset + bsize - 1)]))
         else
-            push!(batches, group[offset:offset + bsize - 1])
+            push!(batches, group[offset:min(length(group), offset + bsize - 1)])
         end
         offset += bsize
     end
+    batches
 end
