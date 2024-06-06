f4739fc90a9e9b63013412fe2c9b65382260041e
Siddhant Chaudhary <urssidd@gmail.com>
2024-06-06 16:08:16 +0530

    Completing the implementation of the `encode_passages` function.

--- src/indexing/collection_encoder.jl
+++ src/indexing/collection_encoder.jl
@@ -8,5 +8,18 @@ end
 function encode_passages(encoder::CollectionEncoder, passages::Vector{String})
     @info "Encoding $(length(passages)) passages."
 
-    # TODO: complete this implementation!
+    if length(passages) == 0
+        error("The list of passages to encode is empty!")
+    end
+
+    embs, doclens = Vector{Matrix}(), Vector{Int}()
+    # batching here to avoid storing intermediate embeddings on GPU
+    # batching also occurs inside docFromText to do batch packing optimizations
+    for passages_batch in ColBERT.batch(passages, encoder.config.indexing_settings.index_bsize * 50)
+        embs_, doclens_ = ColBERT.docFromText(encoder.checkpoint, passages_batch, encoder.config.indexing_settings.index_bsize)
+        push!(embs, embs_)
+        append!(doclens, vec(doclens_))
+    end
+    embs = cat(embs..., dims=2)
+    embs, doclens
 end
