beb65be776f761edd93b2ae4473e3fb69db72e01
Siddhant Chaudhary <urssidd@gmail.com>
2024-06-14 22:07:05 +0530

    Adding fields in the `ResidualCodec` type. Using `@proto` during dev.

--- src/indexing/codecs/residual.jl
+++ src/indexing/codecs/residual.jl
@@ -1,2 +1,10 @@
-struct ResidualCodec
+using ..ColBERT: ColBERTConfig 
+using ProtoStructs
+
+@proto mutable struct ResidualCodec
+    config::ColBERTConfig
+    centroids::Matrix{Float64}
+    avg_residual:: Float64
+    bucket_cutoffs::Vector{Float64}
+    bucket_weights::Vector{Float64}
 end
