763636c3215cbaa828bc4b6299dae84178793dc2
Siddhant Chaudhary <urssidd@gmail.com>
2024-07-15 06:12:50 +0530

    Constructor for `QueryTokenizer` to get the `Q_marker_token_id`.

--- src/modelling/tokenization/query_tokenization.jl
+++ src/modelling/tokenization/query_tokenization.jl
@@ -4,3 +4,8 @@ struct QueryTokenizer
     Q_marker_token_id::Int
     config::ColBERTConfig
 end
+
+function QueryTokenizer(tokenizer::Transformers.TextEncoders.AbstractTransformerTextEncoder, config::ColBERTConfig)
+    Q_marker_token_id = TextEncodeBase.lookup(tokenizer.vocab, config.tokenizer_settings.query_token_id)
+    QueryTokenizer(Q_marker_token_id, config)
+end
