9a73a821d198567da09596cfccaf7bf0455b84d5
Siddhant Chaudhary <urssidd@gmail.com>
2024-07-28 18:43:57 +0530

    Updating the search example.

--- examples/searching.jl
+++ examples/searching.jl
@@ -6,39 +6,20 @@ dataset = "lifestyle"
 datasplit = "dev"
 path = joinpath(dataroot, dataset, datasplit, "short_collection.tsv")
 
-collection = Collection(path)
-length(collection.data)
-
 nbits = 2   # encode each dimension with 2 bits
-doc_maxlen = 300   # truncate passages at 300 tokens
 
-checkpoint = "colbert-ir/colbertv2.0"                       # the HF checkpoint
 index_root = "experiments/notebook/indexes"
 index_name = "short_$(dataset).$(datasplit).$(nbits)bits"
 index_path = joinpath(index_root, index_name)
 
-config = ColBERTConfig(
-    RunSettings(
-        experiment="notebook",
-    ),
-    TokenizerSettings(),
-    ResourceSettings(
-        checkpoint=checkpoint,
-        collection=collection,
-        index_name=index_name,
-    ),
-    DocSettings(
-        doc_maxlen=doc_maxlen,
-    ),
-    QuerySettings(),
-    IndexingSettings(
-        index_path=index_path,
-        index_bsize=3,
-        nbits=nbits,
-        kmeans_niters=20,
-    ),
-    SearchSettings(),
-)
-
 # build the searcher
-searcher = Searcher(config)
+searcher = Searcher(index_path)
+
+# search for a query
+query = "what are white spots on raspberries?"
+pids, scores = search(searcher, query, 2)
+print(searcher.config.resource_settings.collection.data[pids])
+
+query = "are rabbits easy to housebreak?"
+pids, scores = search(searcher, query, 9)
+print(searcher.config.resource_settings.collection.data[pids])
