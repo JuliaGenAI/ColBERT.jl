f0b941af94888216860920e770a923c77f0ba329
Siddhant Chaudhary <urssidd@gmail.com>
2024-08-12 17:15:27 +0530

    Removing unnecessary util functions.

--- src/utils/utils.jl
+++ src/utils/utils.jl
@@ -1,39 +1,4 @@
 """
-    batch(group::Vector, bsize::Int; [provide_offset::Bool = false])
-
-Create batches of data from `group`.
-
-Each batch is a subvector of `group` with length equal to `bsize`. If `provide_offset` is true, each batch will be a tuple containing both the offset and the subvector, otherwise only the subvector will be returned.
-
-# Arguments
-
-  - `group::Vector`: The input vector from which to create batches.
-  - `bsize::Int`: The size of each batch.
-  - `provide_offset::Bool = false`: Whether to include the offset in the output batches. Defaults to `false`.
-
-# Returns
-
-A vector of tuples, where each tuple contains an offset and a subvector, or just a vector containing subvectors, depending on the value of `provide_offset`.
-"""
-function batch(group::Vector, bsize::Int; provide_offset::Bool = false)
-    vtype = provide_offset ?
-            Vector{Tuple{Int, typeof(group)}} :
-            Vector{typeof(group)}
-    batches = vtype()
-    offset = 1
-    while offset <= length(group)
-        if provide_offset
-            push!(batches, (offset, group[offset:min(length(group), offset + bsize - 1)]))
-        else
-            push!(batches, group[offset:min(length(group), offset + bsize - 1)])
-        end
-        offset += bsize
-    end
-    batches
-end
-
-
-"""
     _sort_by_length(
         integer_ids::AbstractMatrix{Int32}, integer_mask::AbstractMatrix{Bool}, bsize::Int)
 
@@ -65,31 +30,4 @@ function _sort_by_length(
     reverse_indices = sortperm(indices)                     # invert the indices list
 
     integer_ids[:, indices], integer_mask[:, indices], reverse_indices
-end
-
-"""
-    _split_into_batches(
-        integer_ids::AbstractMatrix{Int32}, integer_mask::AbstractMatrix{Bool}, bsize::Int)
-
-Split the given `integer_ids` and `integer_mask` into batches of size `bsize`.
-
-# Arguments
-
-  - `integer_ids`: The array of token IDs to batch.
-  - `integer_mask`: The array of attention masks to batch.
-
-# Returns
-
-Batches of token IDs and attention masks, with each batch having size `bsize` (with the possibility of the last batch being smaller).
-"""
-function _split_into_batches(
-        integer_ids::AbstractMatrix{Int32}, integer_mask::AbstractMatrix{Bool}, bsize::Int)
-    batch_size = size(integer_ids)[2]
-    batches = Vector{Tuple{AbstractMatrix{Int32}, AbstractMatrix{Bool}}}()
-    for offset in 1:bsize:batch_size
-        push!(batches,
-            (integer_ids[:, offset:min(batch_size, offset + bsize - 1)],
-                integer_mask[:, offset:min(batch_size, offset + bsize - 1)]))
-    end
-    batches
 end
