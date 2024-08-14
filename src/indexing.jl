951522659ad49f0043933428f1a45272665f70f5
Siddhant Chaudhary <urssidd@gmail.com>
2024-08-14 14:31:00 +0530

    Changing the internals of `Indexer`, and adding a new constructor.

--- src/indexing.jl
+++ src/indexing.jl
@@ -1,32 +1,44 @@
 struct Indexer
     config::ColBERTConfig
+    checkpoint::Checkpoint
+    collection::Vector{String}
 end
 
-function index(indexer::Indexer)
-    index_path = indexer.config.index_path
-    if isdir(index_path)
-        @info "Index at $(index_path) already exists! Skipping indexing."
-        return
-    end
+function Indexer(config::ColBERTConfig)
+    base_colbert = BaseColBERT(config)
+    checkpoint = Checkpoint(base_colbert, config)
+    collection = readlines(config.collection)
 
-    config = indexer.config
-    checkpoint = config.checkpoint
+    @info "Loaded ColBERT layers from the $(checkpoint) HuggingFace checkpoint."
+    @info "Loaded $(length(collection)) documents from $(config.collection)."
 
-    # loading the models
-    @info "Loading ColBERT layers from HuggingFace."
-    base_colbert = BaseColBERT(checkpoint, config)
-    checkPoint = Checkpoint(base_colbert, DocTokenizer(base_colbert.tokenizer, config),
-        QueryTokenizer(base_colbert.tokenizer, config), config)
-
-    # creating the encoder, saver and indexer
-    encoder = CollectionEncoder(config, checkPoint)
-    saver = IndexSaver(config = config)
-    collection_indexer = CollectionIndexer(config, encoder, saver)
-
-    # building the index
-    @info "Building the index."
-    setup(collection_indexer)
-    train(collection_indexer)
-    index(collection_indexer)
-    finalize(collection_indexer)
+    Indexer(config, checkpoint, collection)
 end
+
+# function index(indexer::Indexer)
+#     if isdir(indexer.config.index_path)
+#         @info "Index at $(indexer.config.index_path) already exists! Skipping indexing."
+#         return
+#     end
+#
+#     config = indexer.config
+#     checkpoint = config.checkpoint
+#
+#     # loading the models
+#     @info "Loading ColBERT layers from HuggingFace."
+#     base_colbert = BaseColBERT(checkpoint, config)
+#     checkPoint = Checkpoint(base_colbert, DocTokenizer(base_colbert.tokenizer, config),
+#         QueryTokenizer(base_colbert.tokenizer, config), config)
+#
+#     # creating the encoder, saver and indexer
+#     encoder = CollectionEncoder(config, checkPoint)
+#     saver = IndexSaver(config = config)
+#     collection_indexer = CollectionIndexer(config, encoder, saver)
+#
+#     # building the index
+#     @info "Building the index."
+#     setup(collection_indexer)
+#     train(collection_indexer)
+#     index(collection_indexer)
+#     finalize(collection_indexer)
+# end
