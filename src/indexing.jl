78dc814de594e16c65d234e7e7fc05f6039f5ed1
Siddhant Chaudhary <urssidd@gmail.com>
2024-08-14 14:32:13 +0530

    Keeping the index function; will change it later.

--- src/indexing.jl
+++ src/indexing.jl
@@ -15,30 +15,30 @@ function Indexer(config::ColBERTConfig)
     Indexer(config, checkpoint, collection)
 end
 
-# function index(indexer::Indexer)
-#     if isdir(indexer.config.index_path)
-#         @info "Index at $(indexer.config.index_path) already exists! Skipping indexing."
-#         return
-#     end
-#
-#     config = indexer.config
-#     checkpoint = config.checkpoint
-#
-#     # loading the models
-#     @info "Loading ColBERT layers from HuggingFace."
-#     base_colbert = BaseColBERT(checkpoint, config)
-#     checkPoint = Checkpoint(base_colbert, DocTokenizer(base_colbert.tokenizer, config),
-#         QueryTokenizer(base_colbert.tokenizer, config), config)
-#
-#     # creating the encoder, saver and indexer
-#     encoder = CollectionEncoder(config, checkPoint)
-#     saver = IndexSaver(config = config)
-#     collection_indexer = CollectionIndexer(config, encoder, saver)
-#
-#     # building the index
-#     @info "Building the index."
-#     setup(collection_indexer)
-#     train(collection_indexer)
-#     index(collection_indexer)
-#     finalize(collection_indexer)
-# end
+function index(indexer::Indexer)
+    if isdir(indexer.config.index_path)
+        @info "Index at $(indexer.config.index_path) already exists! Skipping indexing."
+        return
+    end
+
+    config = indexer.config
+    checkpoint = config.checkpoint
+
+    # loading the models
+    @info "Loading ColBERT layers from HuggingFace."
+    base_colbert = BaseColBERT(checkpoint, config)
+    checkPoint = Checkpoint(base_colbert, DocTokenizer(base_colbert.tokenizer, config),
+        QueryTokenizer(base_colbert.tokenizer, config), config)
+
+    # creating the encoder, saver and indexer
+    encoder = CollectionEncoder(config, checkPoint)
+    saver = IndexSaver(config = config)
+    collection_indexer = CollectionIndexer(config, encoder, saver)
+
+    # building the index
+    @info "Building the index."
+    setup(collection_indexer)
+    train(collection_indexer)
+    index(collection_indexer)
+    finalize(collection_indexer)
+end
