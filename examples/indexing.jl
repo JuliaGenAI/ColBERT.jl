7c9894068a1a12c0514b96e0d7e2d7472f2dee5a
Siddhant Chaudhary <urssidd@gmail.com>
2024-08-15 23:23:19 +0530

    Updating the example indexing script.

--- examples/indexing.jl
+++ examples/indexing.jl
@@ -11,14 +11,8 @@ config = ColBERTConfig(
     collection = "./cityofaustin",
     doc_maxlen = 300,
     index_path = "./cityofaustin_index/",
-    chunksize = 500,
+    chunksize = 500
 )
 
 indexer = Indexer(config)
-checkpoint = indexer.checkpoint
-collection = indexer.collection
-
-@time ColBERT.setup(config, checkpoint, collection)
-@time ColBERT.train(config)
-@time ColBERT.index(config, checkpoint, collection)
-@time ColBERT.finalize(config.index_path)
+index(indexer)
