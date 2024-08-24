9b2e01c5368170d92af4c8059229cc69cb9497e2
Siddhant Chaudhary <urssidd@gmail.com>
2024-08-24 19:13:11 +0530

    More structural changes to the indexing code.

--- src/indexing.jl
+++ src/indexing.jl
@@ -46,40 +46,29 @@ function index(indexer::Indexer)
 
     # getting and saving the indexing plan
     isdir(indexer.config.index_path) || mkdir(indexer.config.index_path)
-    plan_dict = setup(indexer.config, indexer.checkpoint, indexer.collection)
-    sample_path = joinpath(indexer.config.index_path, "sample.jld2")
-    @info "Saving sampled embeddings to $(sample_path)."
-    JLD2.save_object(sample_path, plan_dict["local_sample_embs"])
+    sample, sample_heldout, plan_dict = setup(
+        indexer.config, indexer.checkpoint, indexer.collection)
     @info "Saving the index plan to $(joinpath(indexer.config.index_path, "plan.json"))."
     open(joinpath(indexer.config.index_path, "plan.json"), "w") do io
         JSON.print(io,
-            Dict(
-                "chunksize" => plan_dict["chunksize"],
-                "num_chunks" => plan_dict["num_chunks"],
-                "num_partitions" => plan_dict["num_partitions"],
-                "num_embeddings_est" => plan_dict["num_embeddings_est"],
-                "avg_doclen_est" => plan_dict["avg_doclen_est"]
-            ),
+            plan_dict,
             4                                                               # indent
         )
     end
     @info "Saving the config to the indexing path."
     ColBERT.save(indexer.config)
-    # plan_dict["local_sample_embs"] = nothing
-    # GC.gc()
 
     # training/clustering
-    @info "Loading the sample embeddings."
-    sample, heldout = _concatenate_and_split_sample(indexer.config.index_path)
     @assert sample isa AbstractMatrix{Float32} "$(typeof(sample))"
-    @assert heldout isa AbstractMatrix{Float32} "$(typeof(heldout))"
+    @assert sample_heldout isa AbstractMatrix{Float32} "$(typeof(sample_heldout))"
     @info "Training the clusters."
-    codec = train(sample, heldout, plan_dict["num_partitions"],
+    centroids, bucket_cutoffs, bucket_weights, avg_residual = train(
+        sample, sample_heldout, plan_dict["num_partitions"],
         indexer.config.nbits, indexer.config.kmeans_niters)
-    save_codec(indexer.config.index_path, codec["centroids"], codec["bucket_cutoffs"],
-        codec["bucket_weights"], codec["avg_residual"])
-    # sample, heldout = nothing, nothing
-    # GC.gc()
+    save_codec(
+        indexer.config.index_path, centroids, bucket_cutoffs,
+        bucket_weights, avg_residual)
+    sample, sample_heldout, centroids = nothing, nothing, nothing           # these are big arrays
 
     # indexing
     @info "Building the index."
